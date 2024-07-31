da6506699e5241f906be276076861faf17899695
xinjie-just <327460773@qq.com>
2024-08-01 00:24:11 +0800

    init: 程序初始化

---
+++ mini-program/custom-tab-bar/data.js
@@ -0,0 +1,18 @@
+export default [
+    {
+      icon: 'discount',
+      text: '0 元购',
+      url: 'pages/freePurchase/index',
+    },
+    {
+      icon: 'root-list',
+      text: '许愿池',
+      url: 'pages/wishingWell/index',
+    },
+    {
+      icon: 'user',
+      text: '我的',
+      url: 'pages/mine/index',
+    },
+  ];
+  
