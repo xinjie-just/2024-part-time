ff147a44792b4a027eacf695fcafe8c379abefa0
xinjie-just <327460773@qq.com>
2024-08-07 21:29:54 +0800

    feat: 石头剪刀布出拳逻辑开发

--- mini-program/pages/free-purchase/p-k-field/components/rock-paper-scissors/index.js
+++ mini-program/pages/free-purchase/p-k-field/components/rock-paper-scissors/index.js
@@ -1,4 +1,7 @@
 // pages/free-purchase/p-k-field/components/rock-paper-scissors/index.js
+
+import Toast from "/@vant/weapp/toast/toast";
+
 Component({
   options: {
     styleIsolation: 'shared',
@@ -7,15 +10,16 @@ Component({
   /**
    * 组件的属性列表
    */
-  properties: {
+  // properties: {
 
-  },
+  // },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    showExplain: false,
+    result: 3, // 1 我胜，2 对方胜，3 等待出拳，4 对方思考中
   },
 
   /**
@@ -26,6 +30,32 @@ Component({
       wx.switchTab({
         url: '/pages/free-purchase/index',
       })
-    }
+    },
+    onSelectFinish() {
+      this.setData({
+        reslut: 4,
+      });
+      Toast({
+        type: 'loading',
+        message: '对方思考中...',
+        onClose: () => {
+          Toast({
+            type: 'text',
+            message: '对方已出拳',
+            onClose: () => {
+              this.setData({
+                reslut: 1
+              })
+            },
+          });
+        },
+      });
+    },
+    onViewExplain() {
+      this.setData({ showExplain: true });
+    },
+    onCloseExplain() {
+      this.setData({ showExplain: false });
+    },
   }
 })
