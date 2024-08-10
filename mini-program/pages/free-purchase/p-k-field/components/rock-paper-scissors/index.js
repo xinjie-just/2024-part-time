3d9c38c7269a51d827f206ea4c5530c567bbfc16
xinjie-just <327460773@qq.com>
2024-08-10 14:01:41 +0800

    feat: 石头剪刀布游戏页面开发

--- mini-program/pages/free-purchase/p-k-field/components/rock-paper-scissors/index.js
+++ mini-program/pages/free-purchase/p-k-field/components/rock-paper-scissors/index.js
@@ -1,6 +1,7 @@
 // pages/free-purchase/p-k-field/components/rock-paper-scissors/index.js
 
 import Toast from "/@vant/weapp/toast/toast";
+import Dialog from '/@vant/weapp/dialog/dialog';
 
 Component({
   options: {
@@ -18,6 +19,36 @@ Component({
    * 组件的初始数据
    */
   data: {
+    steps: [
+      {
+        text: '首先：',
+        desc: '请先查看游戏规则',
+      },
+      {
+        text: '然后：',
+        desc: '准备好后，点击“开始游戏”，系统将为你匹配对手',
+      },
+    ],
+    icon: {
+      rock: {
+        default: "/assets/images/free-purchase/rock.png",
+        own: "/assets/images/free-purchase/rock-own.png",
+        other: "/assets/images/free-purchase/rock-other.png"
+      },
+      scissor: {
+        default: "/assets/images/free-purchase/scissor.png",
+        own: "/assets/images/free-purchase/scissor-own.png",
+        other: "/assets/images/free-purchase/scissor-other.png"
+      },
+      paper: {
+        default: "/assets/images/free-purchase/paper.png",
+        own: "/assets/images/free-purchase/paper-own.png",
+        other: "/assets/images/free-purchase/paper-other.png"
+      }
+    },
+    matched: false,
+    ownRadio: null,
+    otherRadio: null,
     showExplain: false,
     result: 3, // 1 我胜，2 对方胜，3 等待出拳，4 对方思考中
   },
@@ -26,28 +57,53 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    onStart() {
+      Toast({
+        type: 'loading',
+        message: '正在为您匹配对手...',
+        onClose: () => {
+          this.setData({
+            matched: true
+          })
+        },
+      });
+    },
     onExit() {
-      wx.switchTab({
-        url: '/pages/free-purchase/index',
+      Dialog.confirm({
+        title: '',
+        message: '您确定要退出吗？',
       })
+        .then(() => {
+          wx.switchTab({
+            url: '/pages/free-purchase/index',
+          })
+        })
+        .catch(() => {
+          // on cancel
+        });
     },
     onSelectFinish() {
       this.setData({
-        reslut: 4,
+        result: 4,
       });
       Toast({
         type: 'loading',
         message: '对方思考中...',
         onClose: () => {
-          Toast({
-            type: 'text',
-            message: '对方已出拳',
-            onClose: () => {
-              this.setData({
-                reslut: 1
-              })
-            },
-          });
+          this.getWatchReault();
+        },
+      });
+    },
+    getWatchReault() {
+      Toast({
+        type: 'text',
+        message: '对方已出拳',
+        onClose: () => {
+          this.setData({
+            result: 2,
+            ownRadio: "1",
+            otherRadio: "3",
+          })
         },
       });
     },
@@ -57,5 +113,15 @@ Component({
     onCloseExplain() {
       this.setData({ showExplain: false });
     },
-  }
+    onChangeOwn(e) {
+      this.setData({
+        ownRadio: e.detail,
+      });
+    },
+    onChangeOther(e) {
+      this.setData({
+        otherRadio: e.detail,
+      });
+    },
+  },
 })
